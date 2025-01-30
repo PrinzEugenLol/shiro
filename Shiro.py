9000f3085646ded8bf4da5b0165ed0d8f7567505
PrinzEugenLol <140409773+PrinzEugenLol@users.noreply.github.com>
2025-01-30 22:53:48 +0300

    Update Shiro.py

--- Shiro.py
+++ Shiro.py
@@ -87,7 +87,7 @@ class Shiro(loader.Module):
                             await msg.click(3)
                             break
                             
-                await asyncio.sleep(random.randint(3600, 4200))
+                await asyncio.sleep(random.randint(3300, 3720))
 
 
     async def мувcmd(self, message):
